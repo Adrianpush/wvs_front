a955840a39b4977b21b0f3f074952822a9610484
Adrian Puscuta <adrian.puscu@gmail.com>
2023-07-28 22:43:39 +0300

    Front for WVS

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { CountryExplorer } from "@/components";
 export default function Home() {
   return (
     <main className="flex px-8"  >
-      <div 
-      style={{ backgroundImage: "url('/images/earth.jpg')" }}
-      className="bw-full">
+      <div>
         <h1> Hello There!</h1>
       </div>
 
